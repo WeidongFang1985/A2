db9cd0bcb22b264786c9e93d8ffc9519d489bb9f
WeidongFang1985 <s3914675@student.rmit.edu.au>
2022-05-02 19:01:21 +1000

    Second commit

--- src/MyTiSystem.java
+++ src/MyTiSystem.java
@@ -9,6 +9,7 @@ public class MyTiSystem {
     public static void main(String[] args) {
         TestDatabase ob = new TestDatabase();
         ob.readDatabase();
+
         for (String i: ob.id_list) {
             System.out.println(i);
         }
